4518103a937eafd0b53c3382a1f0ab5bb2769185
insecurity22@naver.com <insecurity22@naver.com>
2017-04-02 14:21:10 -0700

    Update pcap_test.c

--- pcap_test.c
+++ pcap_test.c
@@ -5,98 +5,84 @@
 #include <pcap.h>
 #include <errno.h>
 #include <arpa/inet.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
 
-#define ETHER_ADDR_LEN 6
-
-
-struct libnet_ether_addr {
-        u_int8_t ether_addr_octet[6];
-};
-
-struct libnet_ethernet_hdr {
-        u_int8_t ether_dhost[ETHER_ADDR_LEN];
-        u_int8_t ether_shost[ETHER_ADDR_LEN]; 
-        u_int16_t ether_type; 
-};
+struct ip *iph;
+struct tcphdr *tcph;
 
 
-struct libnet_ipv4_hdr {
-
-u_int8_t ip_hl:4; 
-struct in_addr ip_src;
-struct in_addr ip_dst; 
-};
-
-
-struct libnet_tcp_hdr {
-        u_int16_t th_sport;
-        u_int16_t th_dport; 
-};
-
-
-struct libnet_ipv4_hdr *iph;
-struct libnet_tcp_hdr *tcph;
+void eth_cal(unsigned char *packet) {
+    for(int i=0; i<6; i++) {
+    printf("%02x", packet[i]);
+    if(i!=5) printf(":");
+    }
+    printf("] \n");
+}
 
 
 void callback(u_char *useless, const struct pcap_pkthdr *pkthdr,
                 const u_char *packet)
 {
-    struct libnet_ethernet_hdr *ep;
+    struct ether_header *ep;
     unsigned short ether_type;
+    int cnt = 0;
 
-    ep = (struct libnet_ethernet_hdr *)packet;
-    packet += sizeof(struct libnet_ethernet_hdr);
+    ep = (struct ether_header *)packet;
     ether_type = ntohs(ep->ether_type);
 
-	printf("\n***** Ethernet *****\n");
-	printf("Src mac : [");
-	for(int i=0; i<6; i++) {
-	printf("%02x", ep->ether_shost[i]);
-	if(i!=5) printf(":");
-	}
-	printf("] \n");
 
-	printf("Dst mac : [");
-	for(int i=0;i<6;i++) {
-	printf("%02x", ep->ether_dhost[i]);
-	if(i!=5) printf(":");}
-	printf("]\n");
+    printf("\n***** Ethernet *****\n");
+    printf("Src mac : [");
+    eth_cal(ep->ether_shost);
 
-      
-	iph = (struct libnet_ipv4_hdr *)packet;
-	printf("\n***** Ip header *****\n");
-	printf("Src ip : %s\n", inet_ntoa(iph->ip_src));
-	printf("Dst ip : %s\n", inet_ntoa(iph->ip_dst));
+    printf("Dst mac : [");
+    eth_cal(ep->ether_dhost);
 
 
-	tcph = (struct libnet_tcp_hdr *)(packet + iph->ip_hl * 4);
-	printf("\n***** Tcp header *****\n");
-	printf("Src Port : %d\n" , ntohs(tcph->th_sport));
-	printf("Dst Port : %d\n" , ntohs(tcph->th_dport));
+    packet += sizeof(struct ether_header);
+    iph = (struct ip *)packet;
+    printf("\n***** Ip header *****\n");
+    printf("Src ip : %s\n", inet_ntoa(iph->ip_src));
+    printf("Dst ip : %s\n", inet_ntoa(iph->ip_dst));
 
-	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+    packet += (iph->ip_hl * 4);
+    tcph = (struct tcphdr *)(packet + iph->ip_hl * 4);
+    printf("\n***** Tcp header *****\n");
+    printf("Src Port : %d\n" , ntohs(tcph->th_sport));
+    printf("Dst Port : %d\n" , ntohs(tcph->th_dport));
+
+
+     printf("\n***** Tcp data *****\n");
+     for(int i=0; i<20; i++) {
+     printf("%02x ", packet[i]);
+     cnt++;
+     if((cnt%10)==0) { printf("\n"); }
     }
+
+    printf("\n\n\n\n");
+}
 
 
 int main(int argc, char **argv)
 {
-    char *dev;     
+    char *dev;
     char errbuf[PCAP_ERRBUF_SIZE];
 
-    struct pcap_pkthdr hdr;
     pcap_t *pcd;  // packet capture descriptor
 
-
     if(argc!=2) {
-	printf("usage : %s \"Port 80\"\n", argv[0]);
-	return -1;
-	}
+    printf("usage : %s \"port 80\"\n", argv[0]);
+    return -1;
+    }
 
     dev = pcap_lookupdev(errbuf);
     if (dev == NULL)
     {
         printf("%s\n", errbuf);
-        exit(1);
+        return -1;
     }
     printf("Device : %s\n", dev);
 
@@ -105,7 +91,7 @@ int main(int argc, char **argv)
     if (pcd == NULL)
     {
         printf("%s\n", errbuf);
-        exit(1);
+        return -1;
     }
 
     pcap_loop(pcd, 0, callback, NULL);
