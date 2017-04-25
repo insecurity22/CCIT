7ac88835ddbe8509ee776d985dff7d514581f592
bokyoung <insecurity22@naver.com>
2017-04-26 03:28:15 +0900

    Update WinDivert_test.c Change name, "Makefile" -> "pcap_test2 Makefile"

--- WinDivert_test.c
+++ WinDivert_test.c
@@ -8,11 +8,11 @@
 
 int main(int argc, char **argv) {
 	HANDLE handle, console;
-	UINT i;
+	UINT i, j = 0;
 	INT16 priority = 0;
 	unsigned char packet[MAXBUF];
 	UINT packet_len;
-	WINDIVERT_ADDRESS addr; // 이 구조체는 캡쳐되거나 삽입된 패킷의 주소를 나타낸다.
+	WINDIVERT_ADDRESS addr;
 	PWINDIVERT_IPHDR ip_header = NULL;
 	PWINDIVERT_TCPHDR tcp_header = NULL;
 
@@ -21,8 +21,8 @@ int main(int argc, char **argv) {
 	case 2: break;
 	case 3: break;
 	default:
-		fprintf(stderr, "usage : %s windivert-filter [priority]\n", argv[0]);
-		fprintf(stderr, "example : \t%s true\n", argv[0]);
+		fprintf(stderr, "\nusage : %s windivert-filter [priority]\n", argv[0]);
+		fprintf(stderr, "example : %s true\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
@@ -39,7 +39,6 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
-	// 
 	if (!WinDivertSetParam(handle, WINDIVERT_PARAM_QUEUE_LEN, 8192)) {
 		printf("error : failed to set packet queue length (%d)\n", GetLastError());
 		exit(EXIT_FAILURE);
@@ -57,23 +56,55 @@ int main(int argc, char **argv) {
 			continue;
 		}
 
+		SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_BLUE);
 		WinDivertHelperParsePacket(packet, packet_len, &ip_header, NULL, NULL, NULL, &tcp_header, NULL, NULL, NULL);
-		SetConsoleTextAttribute(console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE);
-
 
 		if (tcp_header != NULL) {
-			printf("TCP : [SrcPort = %u DstPort = %u\nSeqNum = %u AckNum = %u\n"
-				"HdrLength = %u Reserved1 = %u Reserved2 = %u\nUrg = %u Ack = %u"
-				"Psh = %u Rst = %u Syn = %u Fin = %u\nWindow = %u Checksum = 0x%.4X"
-				"UrgPtr=%u]\n", ntohs(tcp_header->SrcPort), ntohs(tcp_header->DstPort),
+			printf("\n\tTCP : [SrcPort = %u DstPort = %u\n\tSeqNum = %u AckNum = %u\n\t"
+				"HdrLength = %u Reserved1 = %u Reserved2 = %u\n\tUrg = %u Ack = %u"
+				"Psh = %u Rst = %u Syn = %u Fin = %u\n\tWindow = %u Checksum = 0x%.4X"
+				"UrgPtr=%u]\n\t", ntohs(tcp_header->SrcPort), ntohs(tcp_header->DstPort),
 				ntohl(tcp_header->SeqNum), ntohl(tcp_header->AckNum), tcp_header->HdrLength,
 				tcp_header->Reserved1, tcp_header->Reserved2, tcp_header->Urg, tcp_header->Ack,
 				tcp_header->Psh, tcp_header->Rst, tcp_header->Syn, tcp_header->Fin,
 				ntohs(tcp_header->Window), ntohs(tcp_header->Checksum), ntohs(tcp_header->UrgPtr));
+			
+			
+			SetConsoleTextAttribute(console, FOREGROUND_GREEN);
+			packet_len = packet_len - sizeof(&ip_header);
+			for (i = 0; i < packet_len; i++) {
+				if (i % 20 == 0) {
+					printf("\n\t");
+				}
+				printf("%.2x ", (UINT8)packet[i]);
+			}
+			printf("\n");
+
+			SetConsoleTextAttribute(console, FOREGROUND_GREEN | FOREGROUND_BLUE);
+			for (i = 0; i < packet_len; i++) {
+				if (i % 40 == 0) {
+					printf("\n\t");
+				}
+				if (isprint(packet[i])){
+					putchar(packet[i]);
+				}
+				else putchar('.');
+			}
+			printf("\n\n\t");
+
+
+			SetConsoleTextAttribute(console, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
+			for (i = 0; i < packet_len; i++) {
+				if (packet[i] == 'H' && packet[i + 1] == 'o' && packet[i + 2] == 's' && packet[i + 3] == 't') {
+					while (!(packet[i] == 0x0d && packet[i + 1] == 0x0a)) {
+						printf("%c", packet[i]);
+						i++;
+					}
+					break;
+				}
+			}
+			printf("\n\n\n\n");
 		}
-
-		printf("\n\n");
-
 	}
 }
 
