9762379c4a28be6ca12fb869574c0d5fb90f76a8
bokyoung <insecurity22@naver.com>
2017-09-21 03:10:11 +0900

    Add my first git code.

---
+++ first/sum_test/main.cpp
@@ -0,0 +1,16 @@
+#include "sum.h"
+#include <iostream>
+using namespace std;
+
+int main() { 
+
+	int i;
+	cin >> i;
+	
+	int j;
+	j=sum(i);
+	cout << j << endl;
+
+	return 0;
+
+}
---
+++ first/sum_test/sum.cpp
@@ -0,0 +1,9 @@
+#include "sum.h"
+
+int sum(int n){
+	int res =0; 
+
+	res = (n*(1+n))/2;
+
+	return res;
+}
