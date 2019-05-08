567eb0c3c967615be02f5a08586abf590979d447
insecurity22 <insecurity22@naver.com>
2019-05-09 07:14:34 +0900

    Update main.cpp

--- arp_spoofing/main.cpp
+++ arp_spoofing/main.cpp
@@ -48,20 +48,20 @@ struct ether_hdr {
 
 struct ether_ip_hdr {
     struct ether_hdr eth;
-    unsigned char ip_v;		            /* version */
-    unsigned char ip_hl;		        /* header length */
-    u_int8_t ip_tos;			        /* type of service */
-    u_short ip_len;			            /* total length */
-    u_short ip_id;			            /* identification */
-    u_short ip_off;			            /* fragment offset field */
-    #define	IP_RF 0x8000			    /* reserved fragment flag */
-    #define	IP_DF 0x4000	    		/* dont fragment flag */
-    #define	IP_MF 0x2000	    		/* more fragments flag */
-    #define	IP_OFFMASK 0x1fff	    	/* mask for fragmenting bits */
-    u_int8_t ip_ttl;        			/* time to live */
-    u_int8_t ip_p;		         	    /* protocol */
-    u_short ip_sum;			            /* checksum */
-    struct in_addr ip_src, ip_dst;	    /* source and dest address */
+    unsigned char ip_v;                 /* version */
+    unsigned char ip_hl;                /* header length */
+    u_int8_t ip_tos;                    /* type of service */
+    u_short ip_len;                     /* total length */
+    u_short ip_id;                      /* identification */
+    u_short ip_off;                     /* fragment offset field */
+    #define	IP_RF 0x8000                /* reserved fragment flag */
+    #define	IP_DF 0x4000                /* dont fragment flag */
+    #define	IP_MF 0x2000                /* more fragments flag */
+    #define	IP_OFFMASK 0x1fff           /* mask for fragmenting bits */
+    u_int8_t ip_ttl;                    /* time to live */
+    u_int8_t ip_p;                      /* protocol */
+    u_short ip_sum;                     /* checksum */
+    struct in_addr ip_src, ip_dst;      /* source and dest address */
 };
 
 struct ifreq ifr;
